7536c2f5096ac27d86be70476089234a95993e39
Daesh <86801468+NetworkingFish@users.noreply.github.com>
2022-04-15 01:06:16 +0800

    Update discordwebhook.go

--- discordwebhook.go
+++ discordwebhook.go
@@ -34,3 +34,25 @@ func SendMessage(url string, message Message) error {
 
 	return nil
 }
+
+func SendEmbed(url string, embed Embed) error {
+	payload := new(bytes.Buffer)
+	err := json.NewEncoder(payload).Encode(message)
+	if err != nil {
+	   return err
+	}
+	resp, err := http.Post(url, "application/json", payload)
+	if err != nil {
+	   return err
+	}
+	if resp.StatusCode != 200 {
+		defer resp.Body.Close()
+		responseBody, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			return err
+		}
+		return fmt.Errorf(string(responseBody))
+	
+	}
+	return nil
+}
